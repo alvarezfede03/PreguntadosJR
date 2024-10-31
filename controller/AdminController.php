14a76a604312da6ca1dc2c91cf023cab29a1e399
mario4796 <mario.checchia@outlook.com>
2024-10-31 01:06:44 -0300

    se le agrego funcionalid minima a la mayoria de los informes

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -26,4 +26,51 @@ class AdminController{
         $data['cantPartidasJugadas'] = $this->model->getCantidadPartidasJugadas();
         $this->presenter->show('informes',$data);
     }
+
+    public function cantidadUsuariosNuevos()
+    {
+        $data['cantUsuariosNuevos'] = $this->model->getCantidadUsuariosNuevos();
+        $this->presenter->show('informes',$data);
+    }
+
+    public function cantidadUsuariosXPais()
+    {
+        $data['cantUsuariosXPais'] = $this->model->getCantidadUsuariosXPais();
+        $data['mostrarUsuariosXPais'] = true;
+        $this->presenter->show('informes',$data);
+    }
+
+    public function cantidadUsuariosXSexo()
+    {
+        $data['cantUsuariosXSexo'] = $this->model->getCantidadusuariosXSexo();
+        $data['mostrarUsuariosXSexo'] = true;
+        $this->presenter->show('informes',$data);
+    }
+
+    public function cantidadUsuariosXGrupoEdad()
+    {
+        $data['cantUsuariosXGrupoEdad'] = $this->model->getCantidadUsuariosXGrupoEdad();
+        $data['mostrarUsuariosXGrupoEdad'] = true;
+        $this->presenter->show('informes',$data);
+    }
+
+    public function porcentajeDePreguntasCorrectas()
+    {
+        $data = [];
+        //Averiguar en base a que se hace el calculo de porcentaje
+        //$data['porcentajePreguntasCorrectas'] = $this->model->getPorcentajePreguntasCorrectas();
+        /*$usuarios  = $this->model->getPorcentajePreguntasCorrectas();
+
+        $data['porcentajePreguntasCorrectas'] = [];
+
+        foreach ($usuarios as $usuario) {
+            $data['porcentajePreguntasCorrectas'][] = [
+                'nombre_usuario' => $usuario['nombre_usuario'],
+                'porcentaje_correctas' => number_format($usuario['porcentaje_correctas'], 2) . '%'
+            ];
+        }
+        $data['mostrarPorcentajePreguntasCorrectas'] = true;*/
+        $this->presenter->show('informes',$data);
+    }
+
 }
--- model/AdminModel.php
+++ model/AdminModel.php
@@ -19,4 +19,52 @@ class AdminModel
         $resultado = $this->database->query($sql);
         return $resultado[0]['count'];
     }
+
+    public function getCantidadUsuariosNuevos(){
+        $sql = "SELECT DATE(fecha_registro) AS dia, COUNT(*) AS count
+                FROM usuarios
+                WHERE fecha_registro >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+                GROUP BY DATE(fecha_registro)
+                ORDER BY dia;";
+        $resultado = $this->database->query($sql);
+        return $resultado[0]['count'];
+    }
+
+    public function getCantidadUsuariosXPais(){
+        $sql = "SELECT pais, COUNT(*) AS total_usuarios
+                FROM usuarios
+                GROUP BY pais
+                ORDER BY total_usuarios DESC;";
+        return $this->database->query($sql);
+    }
+
+    public function getCantidadUsuariosXSexo(){
+        $sql = "SELECT sexo, COUNT(*) AS total_usuarios
+                FROM usuarios
+                GROUP BY sexo
+                ORDER by total_usuarios DESC;";
+        return $this->database->query($sql);
+    }
+
+    public function getCantidadUsuariosXGrupoEdad(){
+        $sql = "SELECT 
+                CASE 
+                    WHEN YEAR(CURDATE()) - YEAR(anio_nacimiento) < 18 THEN 'Menores'
+                    WHEN YEAR(CURDATE()) - YEAR(anio_nacimiento) >= 65 THEN 'Jubilados'
+                ELSE 'Medio'
+                END AS grupo_edad,
+                COUNT(*) AS total_usuarios
+                FROM usuarios
+                GROUP BY grupo_edad
+                ORDER BY grupo_edad;";
+        return $this->database->query($sql);
+    }
+
+    public function getPorcentajePreguntasCorrectas(){
+
+        //Averiguar en base a que se hace el calculo
+        /*
+        $sql = "";
+        return $this->database->query($sql);*/
+    }
 }
